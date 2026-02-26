d15aa6883d94c29e3e862449cd332f90c3c809b3
KMahlangu <mosaprojects1@gmail.com>
2026-02-26 19:41:57 +0200

    Updated the validations

--- Models/Project.cs
+++ Models/Project.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace DevPortfolio.Models;
@@ -7,7 +8,8 @@ public class Project
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     [Required(ErrorMessage = "Project description is required.")]
-    public string Description { get; set; }
+    [DataType(DataType.MultilineText)]
+    public string Description { get; set; } = string.Empty;
     [Required(ErrorMessage = "Please enter a valid URL.")]
     public string? GitHubUrl { get; set; } // e.g., GitHub repo or live demo
 }
