4fb8f3557b485d9923e661989294b5955a48a807
KMahlangu <mosaprojects1@gmail.com>
2026-02-26 11:04:19 +0200

    Added the About method to Home Controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,6 +16,12 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult ServerTime()
+    {
+        var serverTime = DateTime.Now.ToString("F");
+        return Content($"Current server time: {serverTime}");
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
