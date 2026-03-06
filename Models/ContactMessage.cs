fdc06f065c14403517191e39bf968a18009fe653
KMahlangu <mosaprojects1@gmail.com>
2026-03-06 17:41:36 +0200

    Added Validation to ContactMesaegClass

--- Models/ContactMessage.cs
+++ Models/ContactMessage.cs
@@ -7,11 +7,22 @@ namespace DevPortfolio.Models;
 
 public class ContactMessages
 {
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
+    public int Id { get; set; } 
 
+    [Display(Name = "First Name")]
+    [Required(ErrorMessage = "Please enter your name")]
+    public string Name { get; set; } = string.Empty;
+    [Display(Name = "Email")]
+    [Required(ErrorMessage = "Please enter your Email")]
+    [EmailAddress(ErrorMessage = "Enter a valid email address")]
     public string Email { get; set; }
+    [Required(ErrorMessage = "Subject is required")]
+    [StringLength(200, ErrorMessage = "Subject cannot exceed 200 characters")]
     public string Subject { get; set; }
+
+    [Required(ErrorMessage = "Message is required")]
+    [StringLength(1000, MinimumLength = 10, ErrorMessage = "Message must be between 10 and 1000 characters")]
+    [DataType(DataType.MultilineText)]
     public string Message { get; set; }
 
     // Metadata - automatically track when message was sent
