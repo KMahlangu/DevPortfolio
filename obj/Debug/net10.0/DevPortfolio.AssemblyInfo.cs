b8d478442437cab07ea36dd42f179c83399a9851
KMahlangu <mosaprojects1@gmail.com>
2026-03-10 13:38:15 +0200

    Added Skills CRUD in admin

--- obj/Debug/net10.0/DevPortfolio.AssemblyInfo.cs
+++ obj/Debug/net10.0/DevPortfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14958fbd0834ec71e1277131d05a53aa22d17143")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5e4cf0d33e9c13eae534a27e31346ec3923c9db")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
